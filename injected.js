fa24946ef8e602060c6a9c4e3848ff8c03dc744e
Ondřej Hruška <ondra@ondrovo.com>
2016-07-24 09:55:07 +0200

    working save slots and toggle

--- injected.js
+++ injected.js
@@ -4,32 +4,94 @@ var PokemonFilter = new function () {
 	var self = this;
 
 	self.DEBUG = false;
+	self.BUBBLES = false;
 
 	var app = window.App;
 
 	var home = app.home;
 	var pokedex = home.pokedex;
-	//var invPokedex = _.invert(home.pokedex); // for lookup by name
+	var invPokedex = _.invert(home.pokedex); // for lookup by name
+
+	// Pokemon that can't be caught - don't show buttons for them
+	var UNOBTAINABLES = [
+		+invPokedex['Mew'],
+		+invPokedex['Mewtwo']
+	];
 
 	var TOTAL_POKEMON_COUNT = 150;
 
+	//region Config
+
+	self.enabled = true;
+	self.blacklists = [];
+	self.preset_n = 0;
+
+	// This will be a pointer to the active blacklist save slot
 	self.blacklist = [];
 
+	this.loadConfig = function () {
+		var configStr = localStorage.getItem('pokemon_filter');
+
+		// Defaults
+		var config = {
+			enabled: true,
+			blacklists: [[], [], [], [], []],
+			preset_n: 0
+		};
+
+		if (configStr) {
+			try {
+				config = JSON.parse(configStr);
+			} catch (e) {
+				console.warn("PokemonFilter config corrupted, cleaning up.");
+			}
+		}
+
+		console.log(config);
+
+		// Load the config
+		self.blacklists = config.blacklists;
+		if (_.isUndefined(self.blacklists) || !_.isArray(self.blacklists) || self.blacklists.length != 5) {
+			self.blacklists = [[], [], [], [], []];
+		}
+
+		self.preset_n = config.preset_n || 0;
+		self.enabled = !!config.enabled;
+
+		// 'Open the slot'
+		self.blacklist = self.blacklists[self.preset_n];
+
+		console.log("Filter config loaded:\n\t" +
+			"enabled = ", self.enabled,
+			"\n\tblacklists = ", self.blacklists,
+			"\n\tpreset_n = ", self.preset_n);
+	};
+
+	this.persistConfig = function () {
+		var config = {
+			enabled: self.enabled,
+			blacklists: self.blacklists,
+			preset_n: self.preset_n
+		};
+
+		localStorage.setItem('pokemon_filter', JSON.stringify(config));
+	};
+
+	//endregion
+
 	/** Check if a Pokémon is blacklisted */
-	this.isBlacklisted = function(id) {
-		return self.blacklist.indexOf(+id) != -1;
+	this.isBlacklisted = function (id) {
+		return self.enabled && (self.blacklist.indexOf(+id) != -1);
 	};
 
 	/** Start the extension */
-	this.init = function() {
+	this.init = function () {
 		if (_.isUndefined(app) || $('main').hasClass('error')) {
 			console.warn('Either the site scripts changed, or it didn\'t load right.\nCannot init PokéVision Filter.');
 			return;
 		}
 
 		self.loadConfig();
-
-		self.DEBUG && console.log('Blacklist = ',JSON.stringify(self.blacklist));
 
 		self.installFilter();
 		self.applyBlacklist();
@@ -42,12 +104,12 @@ var PokemonFilter = new function () {
 	 * he extension is loaded after the page has already loaded some Pokemon.
 	 * Let's get rid of the hidden ones
 	 */
-	this.applyBlacklist = function() {
+	this.applyBlacklist = function () {
 		_.each(home.pokemon, function (entry) {
 			if (_.isUndefined(entry)) return;
 
 			if (self.isBlacklisted(entry.pokemonId)) {
-				self.DEBUG && console.log('Removing '+pokedex[entry.pokemonId]);
+				self.DEBUG && console.log('Removing ' + pokedex[entry.pokemonId]);
 				// mark it expired - it'll get removed in updateMarkers()
 				entry.expiration_time -= 10000;
 			}
@@ -55,12 +117,33 @@ var PokemonFilter = new function () {
 		home.updateMarkers();
 	};
 
+	/** Select a save slot to use */
+	this.selectActivePreset = function(n) {
+		if (n < 0 || n >= self.blacklists.length) {
+			console.error("Invalid slot number: ", n);
+			return;
+		}
+
+		self.blacklist = self.blacklists[n];
+		self.preset_n = n;
+
+		$('.x-filter-pokemon').each(function(n, elem) {
+			var id = $(elem).data('id');
+			$(elem).toggleClass('filter-hidden', self.isBlacklisted(id));
+		});
+
+		self.persistConfig();
+		self.applyBlacklist();
+
+		self.BUBBLES && app.success('Click the map or refresh the page for it to have effect.', 'Filter preset changed')
+	};
+
 	/** Wrap the createMarker function in a filter */
-	this.installFilter = function() {
+	this.installFilter = function () {
 		home.createMarkerOrig = home.createMarker;
 		home.createMarker = function (idx, pk) {
 			if (self.isBlacklisted(pk.pokemonId)) {
-				self.DEBUG && console.log('Skipping '+pokedex[pk.pokemonId]);
+				self.DEBUG && console.log('Skipping ' + pokedex[pk.pokemonId]);
 				// Return a harmless do-nothing stub
 				return {
 					updateLabel: function () {
@@ -72,22 +155,7 @@ var PokemonFilter = new function () {
 		};
 	};
 
-	this.loadConfig = function() {
-		var str = localStorage.getItem('pokemon_blacklist') || '[]';
-
-		try {
-			self.blacklist = JSON.parse(str);
-		} catch(e) {
-			console.warn("Saved blacklist corrupted, cleaning up.");
-			self.blacklist = [];
-		}
-	};
-
-	this.persistConfig = function() {
-		localStorage.setItem('pokemon_blacklist', JSON.stringify(self.blacklist));
-	};
-
-	this.togglePokemon = function(id, state) {
+	this.togglePokemon = function (id, state) {
 		id = +id; // cast
 
 		self.DEBUG && console.log('Toggling pokemon: ', id);
@@ -98,7 +166,7 @@ var PokemonFilter = new function () {
 			wantShow = state;
 		}
 
-		var elem = $('#filter-pokemon-id-'+id);
+		var elem = $('#filter-pokemon-id-' + id);
 
 		if (wantShow) {
 			// un-hide
@@ -114,25 +182,37 @@ var PokemonFilter = new function () {
 		self.persistConfig();
 	};
 
-	this.createFilterPane = function() {
+	this.createFilterPane = function () {
 		var sidebar = $('.home-sidebar');
 
 		var filterDiv = $('<div class="PokemonFilter"></div>');
 
-		var toggleBox = $('<div class="toggles">' +
-			'<span class="label">Pokémon Filter</span>' +
-			'<a id="pf-hide-all">Hide all</a>' +
-			'<a id="pf-show-all">Show all</a>' +
+		// Top row with toggle buttons
+		var toggleBox = $('<div class="pf-box">' +
+			'<input type="checkbox" id="pf-enable">' +
+			'<label class="pf-title" for="pf-enable">Pokémon Filter</label>' +
 			'</div>');
-
+		for (var i = 0; i < self.blacklists.length; i++) {
+			toggleBox.append('<a class="pf-slot" id="pf-slot-'+i+'" data-n="'+i+'">'+(i+1)+'</a>');
+		}
 		filterDiv.append(toggleBox);
 
-		for (var id = 1; id < TOTAL_POKEMON_COUNT; id++) {
+		// Box with save slots
+		var slotBox = $('<div class="pf-box left">' +
+			'<a class="pf-toggle" id="pf-hide-all">Hide all</a>' +
+			'<a class="pf-toggle" id="pf-show-all">Show all</a>' +
+			'</div>');
+		filterDiv.append(slotBox);
+
+		// Box with the pokémon images
+		for (var id = 1; id < TOTAL_POKEMON_COUNT + 1; id++) {
+			if (UNOBTAINABLES.indexOf(id) != -1) continue;
+
 			var img = document.createElement('img');
 			img.src = 'https://ugc.pokevision.com/images/pokemon/' + id + '.png';
-			img.title = pokedex[id] + ' - '+id+'';
+			img.title = pokedex[id] + ' - ' + id + '';
 			img.classList.add('x-filter-pokemon');
-			img.id = 'filter-pokemon-id-'+id;
+			img.id = 'filter-pokemon-id-' + id;
 			img.dataset.id = id;
 
 			if (self.isBlacklisted(id)) {
@@ -144,26 +224,50 @@ var PokemonFilter = new function () {
 
 		sidebar.prepend(filterDiv);
 
-		$('.x-filter-pokemon').on('click', function() {
-			self.togglePokemon(this.dataset.id);
+		// Preset toggles
+		$('#pf-slot-'+self.preset_n).addClass('active');
+		$('.pf-slot').on('click', function () {
+			$('.pf-slot').removeClass('active');
+			$(this).addClass('active');
+			var n = $(this).data('n');
+			self.selectActivePreset(n);
 		});
 
-		$('#pf-hide-all').on('click', function() {
+		// Enable/disable toggle
+		$('#pf-enable').attr('checked', self.enabled).on('change', function() {
+			self.enabled = $(this).is(':checked');
+			console.log('Filter enabled = ', self.enabled);
+			self.applyBlacklist();
+			self.persistConfig();
+		});
+
+		// Toggle pokémon on click
+		$('.x-filter-pokemon').on('click', function () {
+			self.togglePokemon(this.dataset.id); // also applies and saves
+		});
+
+		// Hide & show buttons
+		$('#pf-hide-all').on('click', function () {
 			$('.x-filter-pokemon').addClass('filter-hidden');
-			self.blacklist = _.range(1, TOTAL_POKEMON_COUNT+1);
+			self.blacklist.length = 0;
+
+			for (var i = 1; i < TOTAL_POKEMON_COUNT+1; i++) {
+				self.blacklist.push(i);
+			}
+
 			self.applyBlacklist();
 			self.persistConfig();
 
-			app.success('Enable the ones you want, then click the map or refresh the page.', 'All species hidden.')
+			self.BUBBLES && app.success('Enable the ones you want, then click the map or refresh the page.', 'All species hidden.')
 		});
 
-		$('#pf-show-all').on('click', function() {
+		$('#pf-show-all').on('click', function () {
 			$('.x-filter-pokemon').removeClass('filter-hidden');
-			self.blacklist = [];
+			self.blacklist.length = 0;
 			self.applyBlacklist();
 			self.persistConfig();
 
-			app.success('Refresh the page or click the map to load them.', 'All species visible.')
+			self.BUBBLES && app.success('Refresh the page or click the map to load them.', 'All species visible.')
 		});
 	};
 };
--- original-script.js
+++ original-script.js
@@ -1,13 +1,178 @@
+// This file is just for reference.
+// It's the de-obfuscated source code of the website we're trying to augment.
+
+;var App = new function () {
+	var self = this;
+	this.adBlock = false;
+	this.lightbox = null;
+	this.init = function () {
+		var body = $('body'), toolt = $('[data-toggle=tooltip]'), tabs = $('[data-toggle=tab]');
+		this.lightbox = lity();
+		toastr.options.newestOnTop = true;
+		toastr.options.progressBar = true;
+		toastr.options.positionClass = 'toast-bottom-left';
+		toolt.tooltip({container: 'body'});
+		tabs.on('click', function () {
+			var t = $(this), e = t.parents('[data-toggle-container]'), n = e.find('[data-toggle=tab]'), i = t.data('target'), s = $(i), o = s.parents('.tabs'), a = o.find('.tab-pane');
+			n.removeClass('is-active');
+			t.addClass('is-active');
+			a.removeClass('is-active');
+			s.addClass('is-active');
+			return false
+		});
+		body.on('click', '[data-href]', function () {
+			var t = $(this), s = t.data('href'), e = t.attr('target');
+			if (e == '_blank') {
+				window.open(s, '_blank')
+			}
+			else {
+				window.location = s
+			}
+			;
+			return false
+		});
+		body.on('click', '[data-lightbox]', this.lightbox)
+	};
+	this.request = function (url, suc, err) {
+		return $.ajax({
+			url: url, type: 'get', dataType: 'json', success: function (t) {
+				if (!t) {
+					err('The response from the server was empty.')
+				}
+				else if (t.status == 'success') {
+					suc(t)
+				}
+				else {
+					err(t.message)
+				}
+			}, error: function (t, s) {
+				err('The response from the server was invalid.')
+			}
+		})
+	};
+	this.postRequest = function (url, data, suc, err) {
+		return $.ajax({
+			url: url, type: 'post', data: data, dataType: 'json', success: function (t) {
+				if (!t) {
+					err('The response from the server was empty.')
+				}
+				else if (t.status == 'success') {
+					suc(t)
+				}
+				else {
+					err(t.message)
+				}
+			}, error: function (t, s) {
+				err('The response from the server was invalid.')
+			}
+		})
+	};
+	this.info = function (text, title) {
+		return toastr.info(text, title)
+	};
+	this.warning = function (text, title) {
+		return toastr.warning(text, title)
+	};
+	this.success = function (text, title) {
+		return toastr.success(text, title)
+	};
+	this.error = function (text, title) {
+		return toastr.error(text, title)
+	};
+	this.isMobile = function () {
+		return $(window).width() < 768
+	};
+	this.checkAdBlock = function () {
+		var e = $('body'), n = $('.ad-unit-hidden'), s = function () {
+			if (n.is(':hidden')) {
+				e.addClass('is-adblocked');
+				return self.adBlock = true
+			}
+			;
+			return false
+		};
+		setTimeout(s, 100);
+		return s()
+	}
+};
+;App.header = new function () {
+	var self = this;
+	this.init = function () {
+		var searchbox = $('.header-map-search'), locate = $('.header-map-locate');
+		searchbox.each(function () {
+			var theBox = $(this), field = theBox.find('[name=name]'), submit = theBox.find('[type=submit]');
+			field.on('keyup', function () {
+				var o = field.val().trim();
+				if (o) {
+					submit.removeAttr('disabled')
+				}
+				else {
+					submit.attr('disabled', true)
+				}
+			});
+			theBox.on('submit', function () {
+				var o = encodeURIComponent(field.val().trim());
+				field.attr('disabled', true);
+				submit.attr('disabled', true);
+				submit.html('<span class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span>');
+				App.request('/map/lookup/' + o, function (o) {
+					field.removeAttr('disabled');
+					submit.removeAttr('disabled');
+					submit.html('<span class="glyphicon glyphicon-search"></span>');
+					if (!App.home.initialised) {
+						window.location = '/#/@' + o.latitude + ',' + o.longitude
+					}
+					else {
+						App.home.latitude = o.latitude;
+						App.home.longitude = o.longitude;
+						App.home.map.panTo({lat: o.latitude, lng: o.longitude});
+						App.home.markers.center.setLatLng({lat: o.latitude, lng: o.longitude});
+						App.home.updateMarkers();
+						App.home.findNearbyPokemon(o.latitude, o.longitude);
+						window.location.hash = '#/@' + o.latitude + ',' + o.longitude
+					}
+				}, function (o) {
+					field.removeAttr('disabled');
+					submit.removeAttr('disabled');
+					submit.html('<span class="glyphicon glyphicon-remove-sign"></span>');
+					App.error(o)
+				});
+				return false
+			})
+		});
+		locate.on('click', function () {
+			if (navigator.geolocation) {
+				navigator.geolocation.getCurrentPosition(function (e) {
+					var t = e.coords.latitude, o = e.coords.longitude;
+					App.home.latitude = t;
+					App.home.longitude = o;
+					App.home.map.panTo({lat: t, lng: o});
+					App.home.markers.center.setLatLng({lat: t, lng: o});
+					App.home.updateMarkers();
+					App.home.findNearbyPokemon(t, o);
+					window.location.hash = '#/@' + t + ',' + o
+				}, function (e) {
+					App.error(e.message)
+				})
+			}
+			else {
+				App.error('Your browser doesn\'t support location tracking, sorry!')
+			}
+			;
+			return false
+		})
+	}
+};
 ;App.home = new function () {
-	var e = this;
+	var self = this;
 	this.TIMER_JOB = 2500;
 	this.TIMER_UPDATE = 30000;
 	this.TIMER_ERROR = 30000;
 	this.TIMER_SCAN_ERROR = 30000;
 	this.TIMER_SCAN_DELAY = 30000;
-	this.initialised = !1;
-	this.loading = !1;
-	this.scanning = !1;
+	this.initialised = false;
+	this.loading = false;
+	this.scanning = false;
 	this.loadingTimer = null;
 	this.map = null;
 	this.tooltipElem = null;
@@ -16,18 +181,18 @@
 	this.pokedex = {};
 	this.pokemon = [];
 	this.markers = {};
-	this.init = function (e) {
-		this.initialised = !0;
-		this.latitude = e.latitude || this.latitude;
-		this.longitude = e.longitude || this.longitude;
-		this.pokedex = e.pokedex || this.pokedex;
+	this.init = function (opts) {
+		this.initialised = true;
+		this.latitude = opts.latitude || this.latitude;
+		this.longitude = opts.longitude || this.longitude;
+		this.pokedex = opts.pokedex || this.pokedex;
 		setInterval(this.updateMarkers, 1000)
 	};
 	this.initMap = function () {
 		var n = $('.home-map-wrapper'), o = $('.home-map-scan');
 		this.tooltipElem = $('.home-map-tooltip');
-		this.map = L.map(n.get(0), {center: {lat: this.latitude, lng: this.longitude}, zoom: 17, zoomControl: !1});
-		this.map.attributionControl.setPrefix(!1);
+		this.map = L.map(n.get(0), {center: {lat: this.latitude, lng: this.longitude}, zoom: 17, zoomControl: false});
+		this.map.attributionControl.setPrefix(false);
 		var i = L.Control.extend({
 			options: {position: 'bottomright', marginTop: 0, marginLeft: 0, marginBottom: 0, marginRight: 0}, onAdd: function () {
 				var e = L.DomUtil.create('div', 'esri-leaflet-logo');
@@ -46,112 +211,112 @@
 		L.Icon.Default.imagePath = '/asset/image/leaflet/';
 		this.markers.center = L.marker({lat: this.latitude, lng: this.longitude}).addTo(this.map);
 		this.map.on('click', function (t) {
-			e.markers.center.setLatLng(t.latlng);
-			e.latitude = t.latlng.lat;
-			e.longitude = t.latlng.lng;
-			e.findNearbyPokemon(e.latitude, e.longitude);
-			window.history.replaceState(null, null, '#/@' + e.latitude + ',' + e.longitude)
+			self.markers.center.setLatLng(t.latlng);
+			self.latitude = t.latlng.lat;
+			self.longitude = t.latlng.lng;
+			self.findNearbyPokemon(self.latitude, self.longitude);
+			window.history.replaceState(null, null, '#/@' + self.latitude + ',' + self.longitude)
 		});
 		o.on('click', function () {
-			e.findNearbyPokemon(e.latitude, e.longitude, !0);
-			return !1
+			self.findNearbyPokemon(self.latitude, self.longitude, true);
+			return false
 		}).removeAttr('disabled');
 		this.updateMarkers();
 		this.findNearbyPokemon(this.latitude, this.longitude);
 		setInterval(function () {
-			e.findNearbyPokemon(e.latitude, e.longitude)
+			self.findNearbyPokemon(self.latitude, self.longitude)
 		}, this.TIMER_UPDATE)
 	};
 	this.findNearbyPokemon = function (t, n, s, r) {
 		var o = $('.home-map-loading'), i = o.find('.loading-message'), a = $('.home-map-scan');
-		if (e.loading) {
+		if (self.loading) {
 			return
 		}
 		;
-		if (e.scanning && !r) {
+		if (self.scanning && !r) {
 			return
 		}
 		;
-		if (!e.scanning && e.loadingTimer) {
-			clearInterval(e.loadingTimer);
-			e.loadingTimer = null
+		if (!self.scanning && self.loadingTimer) {
+			clearInterval(self.loadingTimer);
+			self.loadingTimer = null
 		}
 		;
-		e.loading = !0;
+		self.loading = true;
 		if (o.is(':hidden')) {
 			o.addClass('home-map-loading-mini');
 			o.fadeIn(200)
 		}
 		;
 		if (s) {
-			e.scanning = !0;
-			a.attr('disabled', !0);
+			self.scanning = true;
+			a.attr('disabled', true);
 			a.find('strong').html('<span class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span>');
 			return App.request('/map/scan/' + t + '/' + n, function (i) {
-				e.loading = !1;
-				e.loadingTimer = setTimeout(function () {
-					e.findNearbyPokemon(t, n, !1, i.jobId)
-				}, e.TIMER_JOB)
+				self.loading = false;
+				self.loadingTimer = setTimeout(function () {
+					self.findNearbyPokemon(t, n, false, i.jobId)
+				}, self.TIMER_JOB)
 			}, function (o) {
 				if (o.indexOf('{disabled}') > -1) {
-					i.text('Scanning is currently disabled temporarily, retrying in ' + (e.TIMER_ERROR / 1000) + ' seconds.');
+					i.text('Scanning is currently disabled temporarily, retrying in ' + (self.TIMER_ERROR / 1000) + ' seconds.');
 					i.css('display', 'inline-block')
 				}
 				else if (o.indexOf('{scan-throttle}') > -1) {
-					i.text('You already scanned recently, retrying in ' + (e.TIMER_ERROR / 1000) + ' seconds.');
+					i.text('You already scanned recently, retrying in ' + (self.TIMER_ERROR / 1000) + ' seconds.');
 					i.css('display', 'inline-block')
 				}
 				else {
-					i.text('Unable to send scan request due to an internal error, retrying in ' + (e.TIMER_ERROR / 1000) + ' seconds.');
+					i.text('Unable to send scan request due to an internal error, retrying in ' + (self.TIMER_ERROR / 1000) + ' seconds.');
 					i.css('display', 'inline-block')
 				}
 				;
-				e.loading = !1;
-				e.loadingTimer = setTimeout(function () {
-					e.scanning = !1;
-					e.findNearbyPokemon(t, n, !0)
-				}, e.TIMER_SCAN_ERROR)
+				self.loading = false;
+				self.loadingTimer = setTimeout(function () {
+					self.scanning = false;
+					self.findNearbyPokemon(t, n, true)
+				}, self.TIMER_SCAN_ERROR)
 			})
 		}
 		;
 		return App.request('/map/data/' + t + '/' + n + (r ? '/' + r : ''), function (s) {
 			if (s.jobStatus) {
 				if (s.jobStatus == 'failure' || s.jobStatus == 'unknown') {
-					i.text('Unable to scan for pokemon, retrying in ' + (e.TIMER_SCAN_ERROR / 1000) + ' seconds. If this continues to fail then the Pokemon servers are currently unstable or offline.');
+					i.text('Unable to scan for pokemon, retrying in ' + (self.TIMER_SCAN_ERROR / 1000) + ' seconds. If this continues to fail then the Pokemon servers are currently unstable or offline.');
 					i.css('display', 'inline-block');
-					e.loading = !1;
-					e.loadingTimer = setTimeout(function () {
-						e.scanning = !1;
-						e.findNearbyPokemon(t, n, !0)
-					}, e.TIMER_SCAN_ERROR)
+					self.loading = false;
+					self.loadingTimer = setTimeout(function () {
+						self.scanning = false;
+						self.findNearbyPokemon(t, n, true)
+					}, self.TIMER_SCAN_ERROR)
 				}
 				else if (s.jobStatus == 'in_progress') {
 					i.text('');
 					i.hide();
-					e.loading = !1;
-					e.loadingTimer = setTimeout(function () {
-						e.findNearbyPokemon(t, n, !1, r)
-					}, e.TIMER_JOB)
+					self.loading = false;
+					self.loadingTimer = setTimeout(function () {
+						self.findNearbyPokemon(t, n, false, r)
+					}, self.TIMER_JOB)
 				}
 				;
 				return
 			}
 			;
 			for (var d in s.pokemon) {
-				var l = !1;
-				for (var m in e.pokemon) {
-					if (e.pokemon[m].id == s.pokemon[d].id) {
-						l = !0
+				var l = false;
+				for (var m in self.pokemon) {
+					if (self.pokemon[m].id == s.pokemon[d].id) {
+						l = true
 					}
 				}
 				;
 				if (!l) {
-					e.pokemon.push(s.pokemon[d])
+					self.pokemon.push(s.pokemon[d])
 				}
 			}
 			;
-			e.updateMarkers();
-			if (e.scanning) {
+			self.updateMarkers();
+			if (self.scanning) {
 				i.text('');
 				i.hide();
 				App.success('Scan complete! You can re-scan the area for new pokemon that spawn soon.');
@@ -160,65 +325,65 @@
 				setTimeout(function () {
 					a.removeClass('is-on-cooldown');
 					a.removeAttr('disabled')
-				}, e.TIMER_SCAN_DELAY)
+				}, self.TIMER_SCAN_DELAY)
 			}
 			;
-			e.scanning = !1;
-			e.loading = !1;
-			e.loadingTimer = null;
+			self.scanning = false;
+			self.loading = false;
+			self.loadingTimer = null;
 			if (o.is(':visible')) {
 				o.fadeOut(200)
 			}
 		}, function (o) {
 			i.text('Unable to process response. We are aware of this issue and trying to fix. Please try to refresh the page to potentially solve issue!');
 			i.css('display', 'inline-block');
-			e.loading = !1;
-			e.loadingTimer = setTimeout(function () {
-				e.findNearbyPokemon(t, n, !1, r)
-			}, e.TIMER_ERROR)
+			self.loading = false;
+			self.loadingTimer = setTimeout(function () {
+				self.findNearbyPokemon(t, n, false, r)
+			}, self.TIMER_ERROR)
 		})
 	};
 	this.updateMarkers = function () {
-		if (!e.map) {
+		if (!self.map) {
 			return
 		}
 		;
-		for (var i in e.pokemon) {
-			var n = e.pokemon[i], o = n.expiration_time - Math.floor(+new Date() / 1000), t = e.markers['pokemon-' + i];
+		for (var i in self.pokemon) {
+			var n = self.pokemon[i], o = n.expiration_time - Math.floor(+new Date() / 1000), t = self.markers['pokemon-' + i];
 			if (o <= 0) {
 				if (t) {
-					e.map.removeLayer(t);
-					delete e.markers['pokemon-' + i]
+					self.map.removeLayer(t);
+					delete self.markers['pokemon-' + i]
 				}
 				;
-				delete e.pokemon[i];
+				delete self.pokemon[i];
 				continue
 			}
 			;
 			if (!t) {
-				t = e.createMarker(i, n)
+				t = self.createMarker(i, n)
 			}
 			;
-			t.updateLabel(e.secondsToString(o))
+			t.updateLabel(self.secondsToString(o))
 		}
 	};
 	this.createMarker = function (t, i) {
 		var a = $('body'), r = i.expiration_time - Math.floor(+new Date() / 1000), s = L.Icon.Label.extend({options: {iconUrl: '//ugc.pokevision.com/images/pokemon/' + i.pokemonId + '.png', shadowUrl: null, iconSize: new L.Point(48, 48), iconAnchor: new L.Point(0, 0), labelAnchor: new L.Point(10, 26), wrapperAnchor: new L.Point(0, 0), labelClassName: 'home-map-label'}});
-		var o = new s({labelText: e.secondsToString(r)});
+		var o = new s({labelText: self.secondsToString(r)});
 		var n = new L.Marker.Label({lat: i.latitude, lng: i.longitude}, {icon: o});
 		n.on('mouseover', function (t) {
-			var n = $(t.target._icon), o = i.expiration_time - Math.floor(+new Date() / 1000), r = '<strong>' + (e.pokedex[i.pokemonId] || 'Unknown') + '</strong><small>Despawns in ' + e.secondsToString(o);
-			e.tooltipElem.css({top: (n.offset().top - (a.hasClass('embed-map') ? 0 : 96)) + 'px', left: n.offset().left + 'px'});
-			e.tooltipElem.removeAttr('title');
-			e.tooltipElem.attr('data-original-title', r);
-			e.tooltipElem.tooltip({trigger: 'manual', html: !0}).tooltip('show')
+			var n = $(t.target._icon), o = i.expiration_time - Math.floor(+new Date() / 1000), r = '<strong>' + (self.pokedex[i.pokemonId] || 'Unknown') + '</strong><small>Despawns in ' + self.secondsToString(o);
+			self.tooltipElem.css({top: (n.offset().top - (a.hasClass('embed-map') ? 0 : 96)) + 'px', left: n.offset().left + 'px'});
+			self.tooltipElem.removeAttr('title');
+			self.tooltipElem.attr('data-original-title', r);
+			self.tooltipElem.tooltip({trigger: 'manual', html: true}).tooltip('show')
 		});
 		n.on('mouseout', function () {
 			var e = $('.tooltip');
 			e.remove()
 		});
-		e.markers['pokemon-' + t] = n;
-		e.map.addLayer(n);
+		self.markers['pokemon-' + t] = n;
+		self.map.addLayer(n);
 		return n
 	};
 	this.secondsToString = function (e) {
@@ -233,3 +398,5 @@
 		return i + ':' + n
 	}
 };
+
+
